399d75a579aefbf00da79f8606dcc6703c7d9850
Sharon-Predicthr <sharon@predicthr.net>
2025-11-16 20:06:21 +0200

    Add files via upload

---
+++ database /migrations/0003_create_or_update_v_client_emp_department.sql
@@ -0,0 +1,18 @@
+
+/****** Object:  View [dbo].[v_client_emp_department]    Script Date: 16/11/2025 12:24:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('dbo.v_client_emp_department', 'V') IS NOT NULL
+    DROP VIEW dbo.v_client_emp_department;
+GO
+
+CREATE OR ALTER   VIEW [dbo].[v_client_emp_department] AS
+SELECT client_id, emp_id, MIN(department) AS department
+FROM dbo.attendance
+GROUP BY client_id, emp_id, department
+GO
+
