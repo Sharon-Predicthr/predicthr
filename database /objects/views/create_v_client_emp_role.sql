d6e2ecfb44b82b287a1a0c0d84e9546127755374
Sharon-Predicthr <sharon@predicthr.net>
2025-11-17 19:18:32 +0200

    Add files via upload

---
+++ database /objects/views/create_v_client_emp_role.sql
@@ -0,0 +1,19 @@
+
+
+/****** Object:  View [dbo].[v_client_emp_role]    Script Date: 16/11/2025 12:31:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('dbo.v_client_emp_role', 'V') IS NOT NULL
+    DROP VIEW dbo.v_client_emp_role;
+GO
+
+CREATE   VIEW [dbo].[v_client_emp_role] AS
+SELECT client_id, emp_id, MIN(emp_role) AS emp_role
+FROM dbo.attendance
+GROUP BY client_id, emp_id, emp_role
+GO
+
