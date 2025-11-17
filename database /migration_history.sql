f3f381880f009ecc54e0744719bce2f610fdd479
Sharon-Predicthr <sharon@predicthr.net>
2025-11-17 20:32:08 +0200

    Update migration_history.sql

--- database /migration_history.sql
+++ database /migration_history.sql
@@ -1,8 +1,14 @@
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'migration_history')
+-- migration_history.sql
+-- This table tracks all applied migrations and prevents re-applying them.
+
+IF OBJECT_ID(N'dbo.MigrationHistory', N'U') IS NULL
 BEGIN
-    CREATE TABLE migration_history (
+    CREATE TABLE dbo.MigrationHistory (
         Id INT IDENTITY(1,1) PRIMARY KEY,
-        MigrationName NVARCHAR(255),
+        MigrationName NVARCHAR(255) NOT NULL UNIQUE,
+        Checksum NVARCHAR(64) NULL,
+        AppliedBy NVARCHAR(255) NULL,
         AppliedOn DATETIME DEFAULT GETDATE()
     );
 END;
+GO
