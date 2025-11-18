c28a1a244df886262d9f44f064fab3e46e4bfb07
Sharon-Predicthr <sharon@predicthr.net>
2025-11-18 18:33:53 +0200

    Update build-dev-db.sql

--- deploy/database/dev/build-dev-db.sql
+++ deploy/database/dev/build-dev-db.sql
@@ -1,4 +1,16 @@
 PRINT '=== Starting DEV database build ===';
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'PredictHR_DEV')
+BEGIN
+    PRINT 'Creating DEV database...';
+    CREATE DATABASE PredictHR_DEV;
+END
+GO
+
+PRINT 'Switching context to PredictHR_DEV...';
+USE PredictHR_DEV;
+GO
 
 PRINT 'Applying base objects...';
 
@@ -17,11 +29,11 @@ PRINT 'Applying base objects...';
 :r ..\\objects\\tables\\013_risk_interventions_table.sql
 :r ..\\objects\\tables\\014_work_calendar_dept_table.sql
 :r ..\\objects\\tables\\015_work_calendar_table.sql
-  
+
 :r ..\\objects\\views\\001_v_client_emp_department.sql
 :r ..\\objects\\views\\002_v_client_emp_role.sql
 :r ..\\objects\\views\\003_v_client_emp_site.sql
-  
+
 :r ..\\objects\\procedures\\001_usp_infer_work_calendar.sql
 :r ..\\objects\\procedures\\002_usp_build_dept_calendar.sql
 :r ..\\objects\\procedures\\003_usp_detect_legit_absences.sql
