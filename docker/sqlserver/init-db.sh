95534e7b8c07092ffe0183d2a858b30184695240
Sharon-Predicthr <sharon@predicthr.net>
2025-11-18 13:21:11 +0200

    Update init-db.sh

--- docker/sqlserver/init-db.sh
+++ docker/sqlserver/init-db.sh
@@ -1,1 +1,15 @@
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $sa -i /db/build-dev-db.sql
+#!/bin/bash
+echo "Waiting for SQL Server to start..."
+sleep 20
+
+echo "Running database initialization script..."
+
+sqlcmd -S localhost,1433 \
+    -U sa \
+    -P "$MSSQL_SA_PASSWORD" \
+    -i /db/database/deploy/build-dev-db.sql
+
+echo "Database initialization completed."
+
+# שמירת השרת בחיים
+/opt/mssql/bin/sqlservr
