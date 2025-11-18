c8558e2c6ec9ed8d2c8034a76fb74774b9b41bd0
Sharon-Predicthr <sharon@predicthr.net>
2025-11-18 08:25:14 +0200

    Add files via upload

---
+++ database /objects/tables/015_work_calendar_table.sql
@@ -0,0 +1,20 @@
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.work_calendar') AND type = 'U')
+BEGIN
+
+  CREATE TABLE dbo.work_calendar
+  (
+    client_id        NVARCHAR(50) NOT NULL,
+    calendar_date    DATE         NOT NULL,
+    is_workday       BIT          NOT NULL,
+    present_emp      INT          NOT NULL,
+    active_emp       INT          NOT NULL,
+    coverage_pct     FLOAT        NOT NULL,
+    detection_method NVARCHAR(50) NOT NULL,
+    computed_at      DATETIME2(0) NOT NULL,
+    day_of_week      NVARCHAR(20) NOT NULL,
+    PRIMARY KEY (client_id, calendar_date)
+  );
+  CREATE INDEX IX_work_calendar_client_date
+	ON dbo.work_calendar(client_id, calendar_date) ;
+
+END
