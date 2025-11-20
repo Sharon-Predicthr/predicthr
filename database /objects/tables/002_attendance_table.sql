38663b1ff483cd7a7e5a33874fc39e3b64f76ad9
Sharon-Predicthr <sharon@predicthr.net>
2025-11-20 12:43:23 +0200

    Update 002_attendance_table.sql

--- database /objects/tables/002_attendance_table.sql
+++ database /objects/tables/002_attendance_table.sql
@@ -1,8 +1,8 @@
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.attendance') AND type = 'U')
-BEGIN
 SET QUOTED_IDENTIFIER ON;
 GO
-  
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.attendance') AND type = 'U')
+BEGIN 
 CREATE TABLE dbo.attendance
 (
   client_id   NVARCHAR(50)  NOT NULL,
@@ -23,6 +23,6 @@ CREATE INDEX IX_attendance_client_emp_date
 CREATE INDEX IX_attendance_client_date_dept
   ON dbo.attendance(client_id, event_date)
   INCLUDE(emp_id, department);
-
-
 END
+GO
+
