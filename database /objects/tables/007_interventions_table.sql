5d45d73ff687104458f13111812e34fd11f3c682
Sharon-Predicthr <sharon@predicthr.net>
2025-11-20 12:30:18 +0200

    Update 007_interventions_table.sql

--- database /objects/tables/007_interventions_table.sql
+++ database /objects/tables/007_interventions_table.sql
@@ -1,5 +1,7 @@
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.interventions') AND type = 'U')
 BEGIN
+SET QUOTED_IDENTIFIER ON;
+GO
   CREATE TABLE dbo.interventions
   (
     intervention_id  INT IDENTITY(1,1) PRIMARY KEY,
@@ -7,5 +9,6 @@ BEGIN
     long_description NVARCHAR(2000) NOT NULL,
     is_active        BIT            NOT NULL DEFAULT(1)
   );
+
 
 END
