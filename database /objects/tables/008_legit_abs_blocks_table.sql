732fe5e90d82a65e1e7342127eba76ed3f4e9d52
Sharon-Predicthr <sharon@predicthr.net>
2025-11-20 12:50:08 +0200

    Update 008_legit_abs_blocks_table.sql

--- database /objects/tables/008_legit_abs_blocks_table.sql
+++ database /objects/tables/008_legit_abs_blocks_table.sql
@@ -1,8 +1,9 @@
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.legit_abs_blocks') AND type = 'U')
-BEGIN
 SET QUOTED_IDENTIFIER ON;
 GO
   
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.legit_abs_blocks') AND type = 'U')
+BEGIN
+
 CREATE TABLE dbo.legit_abs_blocks
 (
   client_id        NVARCHAR(50)  NOT NULL,
@@ -19,6 +20,7 @@ CREATE TABLE dbo.legit_abs_blocks
 );
   CREATE INDEX IX_legit_abs_blocks_client_emp_block_start
     ON dbo.legit_abs_blocks(client_id, emp_id, block_start);
-
-
 END
+
+GO
+
