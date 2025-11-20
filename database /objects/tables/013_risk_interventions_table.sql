5543e28acf5d831ea73061fb61edc45752f96155
Sharon-Predicthr <sharon@predicthr.net>
2025-11-20 12:52:22 +0200

    Update 013_risk_interventions_table.sql

--- database /objects/tables/013_risk_interventions_table.sql
+++ database /objects/tables/013_risk_interventions_table.sql
@@ -1,7 +1,8 @@
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.risk_interventions') AND type = 'U')
-BEGIN
 SET QUOTED_IDENTIFIER ON;
 GO
+  
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.risk_interventions') AND type = 'U')
+BEGIN
   
   CREATE TABLE dbo.risk_interventions
   (
@@ -15,6 +16,7 @@ GO
   CREATE INDEX IX_risk_interventions_risk_range
     ON dbo.risk_interventions(risk_type, min_score, max_score, priority)
     INCLUDE(intervention_id, is_active);
-
-
 END
+
+GO
+
