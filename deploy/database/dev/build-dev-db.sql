66477b6a8e5460afa861f059592328189f528edd
Sharon-Predicthr <sharon@predicthr.net>
2025-11-27 08:28:07 +0200

    Update build-dev-db.sql
    commented legit objects

--- deploy/database/dev/build-dev-db.sql
+++ deploy/database/dev/build-dev-db.sql
@@ -31,11 +31,11 @@ PRINT 'Applying base objects...';
 :r /db/objects/tables/001_attendance_rejects_table.sql
 :r /db/objects/tables/002_attendance_table.sql
 :r /db/objects/tables/003_calculated_data_table.sql
-:r /db/objects/tables/004_emp_day_legit_table.sql
+--:r /db/objects/tables/004_emp_day_legit_table.sql
 :r /db/objects/tables/005_emp_sessions_table.sql
 :r /db/objects/tables/006_emp_work_calendar_table.sql
 :r /db/objects/tables/007_interventions_table.sql
-:r /db/objects/tables/008_legit_abs_blocks_table.sql
+--:r /db/objects/tables/008_legit_abs_blocks_table.sql
 :r /db/objects/tables/009_report_flight_table.sql
 :r /db/objects/tables/010_report_integrity_table.sql
 :r /db/objects/tables/011_report_workload_table.sql
@@ -57,7 +57,7 @@ PRINT 'Applying base objects...';
 --------------------------------------------------------
 :r /db/objects/stored_procedures/001_usp_infer_work_calendar.sql
 :r /db/objects/stored_procedures/002_usp_build_dept_calendar.sql
-:r /db/objects/stored_procedures/003_usp_detect_legit_absences.sql
+--:r /db/objects/stored_procedures/003_usp_detect_legit_absences.sql
 :r /db/objects/stored_procedures/004_usp_build_emp_work_calendar.sql
 :r /db/objects/stored_procedures/005_usp_update_adjusted_metrics.sql
 :r /db/objects/stored_procedures/006_usp_calc_sessions.sql
