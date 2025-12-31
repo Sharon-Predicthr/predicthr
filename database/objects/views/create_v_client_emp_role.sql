2e4c69e6827c7a823a8fa4594f52c03ac1dd8e31
sharon <sharon@predicthr.net>
2025-12-31 11:47:18 +0200

    Fix GROUP BY clauses in v_client_emp_role and v_client_emp_site views

--- database/objects/views/create_v_client_emp_role.sql
+++ database/objects/views/create_v_client_emp_role.sql
@@ -1,5 +1,4 @@
 
-
 /****** Object:  View [dbo].[v_client_emp_role]    Script Date: 16/11/2025 12:31:57 ******/
 SET ANSI_NULLS ON
 GO
@@ -11,9 +10,10 @@ IF OBJECT_ID('dbo.v_client_emp_role', 'V') IS NOT NULL
     DROP VIEW dbo.v_client_emp_role;
 GO
 
-CREATE   VIEW [dbo].[v_client_emp_role] AS
+CREATE OR ALTER   VIEW [dbo].[v_client_emp_role] AS
 SELECT client_id, emp_id, MIN(emp_role) AS emp_role
 FROM dbo.attendance
-GROUP BY client_id, emp_id, emp_role
+WHERE emp_role IS NOT NULL
+GROUP BY client_id, emp_id
 GO
 
